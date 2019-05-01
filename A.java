8736499b40f874ce530642b78e541f688d7047bd
davdeyang <1320501826@qq.com>
2019-05-02 00:57:14 +0800

    新增文件1个

--- A.java
+++ A.java
@@ -4,6 +4,10 @@ public class A {
 
     public static void main(String args [])
     {
-        System.out.println("test")
+        System.out.println("test");
+    }
+    public void stata()
+    {
+        System.out.println("hello world");
     }
 }
