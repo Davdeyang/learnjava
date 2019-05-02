d09db47eae38d37a4c681e12fa747808835d9395
davdeyang <1320501826@qq.com>
2019-05-02 21:22:56 +0800

    dkk

--- A.java
+++ A.java
@@ -4,7 +4,7 @@ public class A {
 
     public static void main(String args [])
     {
-        System.out.println("test");
+        System.out.println("sdfsf");
     }
     public void stata()
     {
